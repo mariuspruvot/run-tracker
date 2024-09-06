77598f36281f8475297dcfde8fc9639354f89058
mariuspruvot <marius.pruvot@papernest.com>
2024-09-06 22:16:02 +0200

    feat: Introduce PostgreSQL connection setup

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,19 +1,25 @@
+# backend/database.py
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from backend.settings.base import GLOBAL_SETTINGS
 
-engine = create_engine(
-    "sqlite:///./database.db", connect_args={"check_same_thread": False}
-)
+# Create the SQLAlchemy engine
+DATABASE_URL = GLOBAL_SETTINGS.get_database_url()
+
+# Sets up the connection to PostgreSQL
+engine = create_engine(DATABASE_URL, echo=True)
+
+# session factory used to create new session objects that interact with the database.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# All models will inherit from this class.
 Base = declarative_base()
-
 
 def get_db():
     """
-    Create a new database session
-    :return:
+    This function is a generator that provide database sessions
     """
     db = SessionLocal()
     try:
--- backend/utils/init_db.py
+++
@@ -1,6 +0,0 @@
-def create_tables():
-    """
-    Create all database tables defined in the application
-    :return:
-    """
-    pass
